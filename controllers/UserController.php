5f0357d742a4af91604f3c5f2ec0e40e8df36033
03milosevicN <nikolamilosevic220803@gmail.com>
2024-11-13 11:43:20 +0100

    Configured layouts, added DB.

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -10,10 +10,66 @@ class UserController extends BaseController
     public function readUser()
     {
         $model = new UserModel();
-        $model->email = 'pbisevac@singidunum.ac.rs';
-        $model->firstName = 'Nikola';
-        $model->lastName = 'Bisevac';
+        $model->one("where id = 2");
 
         $this->view->render('getUser', 'main', $model);
+    }
+
+    public function readAll()
+    {
+        $model = new UserModel();
+
+        $results = $model->all("");
+
+        $this->view->render('users', 'main', $results);
+    }
+
+    public function updateUser()
+    {
+        $model = new UserModel();
+
+        $model->mapData($_GET);
+
+        $model->one("where id = $model->id");
+
+        $this->view->render('updateUser', 'main', $model);
+    }
+
+    public function processUpdateUser()
+    {
+        $model = new UserModel();
+
+        $model->mapData($_POST);
+
+        $model->update("where id = $model->id");
+
+        header("location:" . "/users");
+    }
+
+
+    public function createUser()
+    {
+
+        $model = new UserModel();
+
+        $this->view->render('createUser', 'main', $model);
+    }
+
+    public function processCreate()
+    {
+        $model = new UserModel();
+
+        $model->mapData($_POST);
+
+        $model->validate();
+
+        if ($model->errors) {
+            $this->view->render('createUser', 'main', $model);
+            exit;
+        }
+
+        $model->insert();
+
+        header("location:" . "/users");
     }
 }
--- views/getUser.php
+++ views/getUser.php
@@ -6,4 +6,8 @@ use app\models\UserModel;
 
 ?>
 
-<h1><?php echo $params->firstName ?> <br> <?php echo $params->lastName?> <br><?php echo $params->email?></h1>
+<div class="card">
+    <div class="card-body">
+        <h1><?php echo $params->first_name ?? "NOT FOUND" ?> <br> <?php echo $params->last_name  ?? "NOT FOUND" ?> <br><?php echo $params->email  ?? "NOT FOUND" ?></h1>
+    </div>
+</div>
